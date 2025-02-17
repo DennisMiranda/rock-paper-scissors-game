740b04c61535beb88f1043377c1434194e9f1406
Jaime Loayza <betoloayma@hotmail.com>
2025-02-17 06:16:17 -0500

    crea maxScore, modal en html y function showModal

--- js/index.js
+++ js/index.js
@@ -5,25 +5,25 @@ const playerImage = document.getElementById('player-image');
 const computerImage = document.getElementById('computer-image');
 let playerScore = 0;
 let computerScore = 0;
+let maxScore = 3;
 
 document.getElementById('rock').addEventListener('click', () => playGame('rock'));
 document.getElementById('paper').addEventListener('click', () => playGame('paper'));
 document.getElementById('scissors').addEventListener('click', () => playGame('scissors'));
 
+document.getElementById('btn-reset').addEventListener('click',() => resetGame());
+document.getElementById('btn-home').addEventListener('click', (event) => {
+    window.location.href = '#'
+});
+
 function playGame(playerChoice) {
     const computerChoice = choices[Math.floor(Math.random() * choices.length)];
-    updateImages(playerChoice, computerChoice);
     determineWinner(playerChoice, computerChoice);
-}
-
-function updateImages(playerChoice, computerChoice) {
-    playerImage.src = `assets/${playerChoice}.svg`;
-    computerImage.src = `assets/${computerChoice}.svg`;
 }
 
 function determineWinner(playerChoice, computerChoice) {
     if (playerChoice === computerChoice) {
-        return; // Empate, no se actualiza el puntaje
+        return; 
     }
     
     if (
@@ -37,4 +37,23 @@ function determineWinner(playerChoice, computerChoice) {
         computerScore++;
         computerScoreElement.textContent = computerScore;
     }
+
+    if (playerScore >= maxScore) {
+        showModal('HUMAN victory!'); 
+    }else if (computerScore >= maxScore){
+        showModal('COMPUTER victory!');
+    }   
+}
+
+function showModal(message) {
+        const modal = new bootstrap.Modal(document.getElementById('winnerModal'));
+        document.getElementById('winnerMessage').textContent = message;
+        modal.show();
+}
+
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreElement.textContent = playerScore;
+    computerScoreElement.textContent = computerScore;
 }
